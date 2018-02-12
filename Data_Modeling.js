ced33ae50e384ec77fc406a70928cfb2b1a6a92f
“FatimaAlmostafa” <ff242468@gmail.com>
2018-02-12 10:30:11 +0200

    “anycommentyouWant”

---
+++ Data_Modeling.js
@@ -0,0 +1,36 @@
+	// 1-create a data model to represent some of your classmates
+	var classmates=("samer", "fatima", "belal")
+	// 	-think of different attributes of your classmates? what do all of them have ?
+	// 	-create a factory function.
+	function ss(name,favourit sport){
+		return
+		name:name;
+		favourit sport: "favourit sport is:"+" "+favourit sport;
+	}
+	
+	// 	-create an array to hold the classmates that you have created.
+	 var classmates2=[];
+	 function dd(i){
+	 	return
+	 	classmates2.push[i]
+	 }
+
+	
+	// 	-write a function called displayFriend that takes a mate as an argument and returns the important information in a readable way.
+	function displayFriend(mate){
+		for (var i=0; i<mate.length; i++)
+			r
+
+
+	}
+
+	// 	-write a function called addFriend that takes a mate as an argument and add it to you classMates array.
+	function addFriend(mate){
+		return classmates2.mate[i]
+
+	}
+	// 	-calculate the number of male friends that your class have by writing a function called nbOfMale.
+
+	// 	-Write a function searchMates that, given a query and an array of Mates,
+
+	//           searche the array of mates for "matching" mate. You will decide what way you want to write in your search algorithm.
