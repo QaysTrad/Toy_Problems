ee31f8b8518648cba8159e1bb7cdc480500800d2
NaderNasr <nadernasr7@gmail.com>
2018-02-14 09:43:29 +0200

    Wednesday 14th

--- HigherFunctionsEach2.js
+++ HigherFunctionsEach2.js
@@ -1,34 +1,48 @@
-/*
-using improved each create function that returns an array with all the names in the array
-if the index is even
-var x = [{name : 'Jon',age : 45}, {name : 'Ali', age : 28},
-{name :'Omar', age :17},{name :'Ola', age :37}, {name 'Salwa', age : 22}];
+
+// using improved each create function that returns an array with all the names in the array
+// if the index is even
+var x = [{name : 'Jon',age : 45}, {name : 'Ali', age : 28},{name :'Omar', age :17},{name :'Ola', age :37}, {name 'Salwa', age : 22}];
+
 pName(x); = > ['Jon', 'Omar', 'salwa']
-*/
+
 	function each(array, func) {
 	  for (var i = 0; i < array.length; i++) {
 	    func(array[i], i);
 	  }
 	}
-    
+    else {
+        for (var k in array) {
+            func(array[k], k)
+        }
+    }
+
 	function pName(argument) {
-		// your code is here
+		var NewArr = []
+        //var i = 0;
+        return map(array, function(func, i){
+            if ( x == % 2) {
+                NewArr.push (element.name)
+            }
+                    return NewArr;
+        })
+
+
 	}
 
+//1) using improved each with objects, create function that print every element inside the object	
 
-/*
-1) using improved each with objects, create function that print every element inside the object	
 var obj_2 = {name: 'Ibrahim', age : 67, phone : '078-0000000'}
-printValue(obj_2); => 
-    Ibrahim
-    67
-    078-0000000
+                                                                // printValue(obj_2); => 
+                                                                //     Ibrahim
+                                                                //     67
+                                                                //     078-0000000
+
 var obj_1 = {name: 'Salim', age : 15, phone : '079-0000000'}
-printValue(obj_1); => 
-    Salim
-    15
-    079-0000000
-*/
+                                                                // printValue(obj_1); => 
+                                                                //     Salim
+                                                                //     15
+                                                                //     079-0000000
+
     function each(coll, func) {
         if (Array.isArray(coll)) {
             for (var i = 0; i < coll.length; i++) {
@@ -45,4 +59,8 @@ printValue(obj_1); =>
     
 	function printValue(obj) {
 		// your code is here
+        return map(coll, function(func, i){
+            return obj_1; //.name x
+                          // obj_1, obj_2 ?
+        })
 	}
