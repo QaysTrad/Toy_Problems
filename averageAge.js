96de4f1615b5231343e05ec65b6b72800e989515
Besslan <besslan.bazadough@gmail.com>
2018-02-19 09:34:02 +0200

    toy

--- averageAge.js
+++ averageAge.js
@@ -11,7 +11,42 @@
        {name: {first: 'Obada', last: 'Eddin'}, age: 24} 
  ]; 
  function averageAge(people) { 
-       // YOUR CODE HERE 
+ 	var res =0
+ 	var count = 0
+     for (var i = 0 ; i<people.length ; i++){
+if (people[i].age >= 18 && people[i].age <= 50) {
+ 
+res = res + people[i].age
+count++
+}
+     }
+     return res/count
  }
+
+// would have solved in another way but i dont know how to use the built in each function or the other ones 
+
+/*function each(array,func){
+
+for (var i = 0 ; i < array.length ; i++){
+func(array[i],i)
+
+}
+}
+
+function averageAge1(people) {
+	var res = 0
+	var count = 0
+each(people,function(x){
+if (people[i].age >= 18 && people[i].age <= 50) {
+
+	res = res + people[i].age
+count++
+}
+
+
+})
+
+return res/count
+}*/
 
  averageAge(people); // 43+36+44+24 = 36.74
