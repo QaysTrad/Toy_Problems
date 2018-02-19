85b0692d875818e9c6d8c3637958274a6e2bcfc9
Atheer83 <atheer.for.tech@gmail.com>
2018-02-19 09:27:02 +0200

    finding the average

--- averageAge.js
+++ averageAge.js
@@ -1,5 +1,23 @@
 // Write a function called  averageAge that accpets an array of objects and return the average ages for the people who are between 18 and 50
  
+function each(coll, func) { 
+       if (Array.isArray(coll)) { 
+             for (var i = 0; i < coll.length; i++) { 
+                   func(coll[i], i); 
+             } 
+       } else { 
+             for (var key in coll) { 
+                   func(coll[key], key); 
+             } 
+       } 
+ }
+function reduce(array, f, start) { 
+       var acc = start; 
+       each(array, function(element) { 
+             acc = f(acc, element); 
+       }); 
+       return acc; 
+ }
 
  var people = [ 
        {name: {first: 'Ahmad', middle: 'B.', last: 'AlAhmad'}, age: 85}, 
@@ -11,7 +29,9 @@
        {name: {first: 'Obada', last: 'Eddin'}, age: 24} 
  ]; 
  function averageAge(people) { 
-       // YOUR CODE HERE 
+       return reduce (people, function (start, element) {
+       		return start + element.age;
+       }, 0) / people.length;
  }
 
  averageAge(people); // 43+36+44+24 = 36.74
