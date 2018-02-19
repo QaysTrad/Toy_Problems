e6075e4bc7c58e36de4fc9c0ff651c87af8c5dac
MohammadJozaa7 <36266902+MohammadJozaa7@users.noreply.github.com>
2018-02-19 09:31:10 +0200

    “anycommentyouWant”

--- averageAge.js
+++ averageAge.js
@@ -1,4 +1,6 @@
-// Write a function called  averageAge that accpets an array of objects and return the average ages for the people who are between 18 and 50
+// Write a function called  averageAge that accpets 
+//an array of objects and return the average ages 
+//for the people who are between 18 and 50
  
 
  var people = [ 
@@ -12,6 +14,23 @@
  ]; 
  function averageAge(people) { 
        // YOUR CODE HERE 
+       var averageAge7=0
+       var number=0
+       for (var i = 0 ; i < people.length; i++) {
+        	if (people[i].age>17 && people[i].age<51 ) {
+			averageAge7= averageAge7+ people[i].age
+			number=number+1
+      	}
+       	      }
+       	      return averageAge7/number
  }
 
  averageAge(people); // 43+36+44+24 = 36.74
+
+ var x= 9999
+
+
+
+
+
+ //thx for easy toy problem
