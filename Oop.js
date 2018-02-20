0341c50ae7c7bee2f6406c64bd846f0c888a7bf3
abeer-shami <abeer.shami887@gmail.com>
2018-02-20 09:26:39 +0200

    toy#6

--- Oop.js
+++ Oop.js
@@ -28,3 +28,42 @@ function makeGame(upperbound){
   	}
   }
 }
+var rand=randInt(n)
+
+function randInt(n){
+
+  function makeGame(upperbound){
+  var upper=upperbound;
+  var rand=randInt(upper)
+  var counter=0;
+
+ rand.guessMyNumber=guessMyNumber
+ rand.giveUp=giveUp
+ rand.numOfGuesses=numOfGuesses
+
+
+  }
+  return Math.floor(Math.random() * (n + 1)); 
+
+}
+
+var guessMyNumber=function(n){
+    counter++;
+      if (n > upper) {
+        return "Out of bounds! Please try a number between 0 and " + upperbound + ".";
+      } else if (n === rand) {
+        return "You guessed my number!";
+      }
+      return "Nope! That wasn't it!";
+    },
+    var giveUp=function(){
+        return rand;
+      },
+      var numOfGuesses=function(){
+          return counter;
+
+      }
+}
+
+
+
