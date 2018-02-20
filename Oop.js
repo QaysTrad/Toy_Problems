5cab13ec98654e053b46f813787f23bde69e70e1
HananNouman <hnouman@hotmail.co.uk>
2018-02-20 09:37:28 +0200

    Oop

--- Oop.js
+++ Oop.js
@@ -28,3 +28,45 @@ function makeGame(upperbound){
   	}
   }
 }
+// answer
+function randInt(n) {
+    return Math.floor(Math.random() * (n + 1));
+}
+
+function makeGame(upperbound){
+  var game={};
+     game.upperbound=upperbound;
+     game.upper=upperbound;
+     game.rand=randInt(game.upper)
+     game.counter=0;
+     game.guessMyNumber=guessMyNumber;
+     game.giveUp=giveUp;
+     game.numOfGuesses=numOfGuesses;
+  
+   return game;
+}
+
+
+var guessMyNumber=function(n){
+      this.counter=this.counter+1;
+      if (n > this.upper) {
+        return "Out of bounds! Please try a number between 0 and " + this.upperbound + ".";
+      } else if (n === this.rand) {
+        return "You guessed my number!";
+      }
+      return "Nope! That wasn't it!";
+    }
+
+
+var giveUp=function(){
+      return this.rand;
+    }
+
+ var numOfGuesses =function(){
+      return this.counter;
+    }
+
+
+
+
+
