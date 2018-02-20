ee7cec632ab014a67ea2efed54ba16be20d37a4e
QaysTrad <qaystrad@gmail.com>
2018-02-20 09:20:58 +0200

    QT Oop

--- Oop.js
+++ Oop.js
@@ -7,24 +7,32 @@ function randInt(n) {
 }
 
 function makeGame(upperbound){
-  var upper=upperbound;
-  var rand=randInt(upper)
-  var counter=0;
-  return {
-  	guessMyNumber:function(n){
-  		counter++;
-	    if (n > upper) {
-	      return "Out of bounds! Please try a number between 0 and " + upperbound + ".";
-	    } else if (n === rand) {
-	      return "You guessed my number!";
-	    }
-	    return "Nope! That wasn't it!";
-  	},
-  	giveUp:function(){
-    	return rand;
-  	},
-  	numOfGuesses:function(){
-    	return counter;
-  	}
-  }
+
+  var game = {}
+  game.upperbound = upperbound
+  game.upper = upperbound;
+  game.rand = randInt(game.upper)
+  game.counter = 0;
+
+  game.guessMyNumber = guessMyNumber ;
+  game.giveUp = giveUp ;
+  game.numOfGuesses = numOfGuesses ; 
+
+  return game 
 }
+  function guessMyNumber(n){
+      this.counter++;
+      if (n > this.upper) {
+        return "Out of bounds! Please try a number between 0 and " + this.upperbound + ".";
+      } else if (n === this.rand) {
+        return "You guessed my number!";
+      }
+      return "Nope! That wasn't it!";
+    }
+
+    function giveUp(){
+      return this.rand;
+    }
+    function numOfGuesses(){
+      return this.counter;
+    }
