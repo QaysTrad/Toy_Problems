a379afc0b4731363cf51590f0316102b00fc542f
AymanGhaith <ayman.ghaith88@gmail.com>
2018-02-20 09:24:31 +0200

    OOP toy problem , refactoring the guess my number function

--- Oop.js
+++ Oop.js
@@ -28,3 +28,36 @@ function makeGame(upperbound){
   	}
   }
 }
+
+function makeGameOOP(upperbound){
+  var gameObj={}
+
+  gameObj.upper=upperbound;
+  //the following line did not work !!
+  //it should access the object's variables !! 
+  //gameObj.rand=randInt(this.upper);
+  gameObj.rand=randInt(upperbound);
+  gameObj.counter=0;
+
+  gameObj.guessMyNumber=function(n){
+      this.counter++;
+      if (n > this.upper) {
+        //return "Out of bounds! Please try a number between 0 and " + this.upper + ".";
+        //I don't know why the following line did work
+        return "Out of bounds! Please try a number between 0 and " + upperbound + ".";
+      } else if (n === this.rand) {
+        return "You guessed my number!";
+      }
+      return "Nope! That wasn't it!";
+    }
+
+    gameObj.giveUp=function(){
+      return this.rand;
+    }
+
+    gameObj.numOfGuesses=function(){
+      return this.counter;
+    }
+
+    return gameObj;
+}
