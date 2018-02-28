9e618e6af135eaab1430e36ca88f12b6c8df27ce
QaysTrad <qaystrad@gmail.com>
2018-02-28 09:19:25 +0200

    QT Swap

--- swap.js
+++ swap.js
@@ -3,4 +3,16 @@
 	// ToYPRoblEm --> tOyprOBLeM
 	function swap(input){
 		//your code is here
+		var newstr = ""
+		for (var i = 0; i < input.length; i++) {
+			
+			if(input[i] === input[i].toLowerCase()){
+				newstr+=input[i].toUpperCase()
+			}
+			else if(input[i] === input[i].toUpperCase()){
+				newstr+=input[i].toLowerCase()
+			}
+		}
+		return newstr
+		
 	}
