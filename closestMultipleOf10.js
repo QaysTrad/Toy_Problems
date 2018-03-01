7349657fc2d036c245bc1234e12a550b07c87bd6
QaysTrad <qaystrad@gmail.com>
2018-03-01 09:27:05 +0200

    QT the Closest

--- closestMultipleOf10.js
+++ closestMultipleOf10.js
@@ -5,4 +5,21 @@
 	// 37 ===> 40
  function closestMultipleOf10(num) {
  	//your code is here
+ 	var newNum = num%10
+ 	var str = num +""
+ 	if(newNum <=5){
+ 		str = str[0] + "0"
+ 		str = parseInt(str ,"<br>")
+ 		return str
+ 	}
+ 	else{
+ 		str = parseInt(str[0] ,"<br>")
+ 		
+ 		str +=1
+ 		str+=""
+ 		str = str[0] + "0"
+ 		str = parseInt(str ,"<br>")
+ 		return str
+ 	}
+ 	
  }
